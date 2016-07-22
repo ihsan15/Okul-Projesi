63f985a0259b16abffae8d04b015484d945f8d42
ihsan15 <isik.ihsan01@gmail.com>
2016-07-22 14:40:11 +0300

    a

---
+++ WebEntityOkul/WebForm1.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebEntityOkul.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Öğrenciler</title>
+    <style type="text/css">
+        @import "okul.css";
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="width:1000px;height:800px">
+    <div class="icgovde">
+
+        <asp:DropDownList ID="ddlSiniflar" runat="server" AutoPostBack="true" OnSelectedIndexChanged="itemselected" Height="21px" Width="218px" OnDataBound="ddlSiniflar_DataBound">
+        </asp:DropDownList>
+                
+        <br />
+        <br />
+                
+        <asp:GridView ID="gvOgrenciler" runat="server" Height="284px" Width="893px" AutoGenerateSelectButton="True" OnSelectedIndexChanged="gvOgrenciler_SelectedIndexChanged" OnDataBound="gvOgrenciler_DataBound" DataKeyNames="Id">
+        </asp:GridView>
+    </div>
+
+        <div class="icgovde">
+            <asp:LinkButton ID="lbtnEkle" runat="server" OnClick="lbtnEkle_Click" >Yeni Öğrenci Ekle</asp:LinkButton>
+            <asp:Panel ID="OgrencilerPaneli" runat="server" Height="300px" Width="893px" Visible="false">
+            <table style="width:500px;margin-left:250px;margin-top:10px;">
+                <tr style="height:30px;">
+                    <td style="text-align:center;width:150px;">Adı</td>
+                    <td><asp:TextBox ID="txtAdi" runat="server" Width="184px"></asp:TextBox></td>
+                    <td><asp:TextBox ID="txtId" runat="server" Width="57px" Visible="False"></asp:TextBox></td>
+                </tr>
+                <tr style="height:30px;">
+                    <td style="text-align:center;width:150px;">Soyadı</td>
+                    <td colspan="2"><asp:TextBox ID="txtSoyadi" runat="server" Width="184px"></asp:TextBox></td>
+                </tr>
+                <tr style="height:30px;">
+                    <td style="text-align:center;width:150px;">Telefon</td>
+                    <td colspan="2"><asp:TextBox ID="txtTelefon" runat="server" Width="184px"></asp:TextBox></td>
+                </tr>
+                <tr style="height:30px;">
+                    <td style="text-align:center;width:150px;">Adres</td>
+                    <td colspan="2"><asp:TextBox ID="txtAdres" runat="server" Width="184px"></asp:TextBox></td>
+                </tr>
+                <tr style="height:30px;">
+                    <td style="text-align:center;width:150px;">TC</td>
+                    <td colspan="2"><asp:TextBox ID="txtTC" runat="server" Width="184px"></asp:TextBox></td>
+                </tr>
+                <tr style="height:30px;">
+                    <td style="text-align:center;width:150px;">Taksit Sayısı</td>
+                    <td colspan="2"><asp:TextBox ID="txtTSayisi" runat="server" Width="184px"></asp:TextBox></td>
+                </tr>
+                <tr style="height:30px;">
+                    <td style="text-align:center;width:150px;">Taksit Tutarı</td>
+                    <td colspan="2" ><asp:TextBox ID="txtTTutari" runat="server" Width="184px"></asp:TextBox></td>
+                </tr>
+                <tr style="height:30px;">
+                    <td><asp:Button ID="btnKaydet" runat="server" Text="Kaydet" Enabled="False" OnClick="btnKaydet_Click" /> </td>
+                    
+                    <td><asp:Button ID="btnDegistir" runat="server" Text="Değiştir" Enabled="False" OnClick="btnDegistir_Click" /></td>
+                    <td style="width:140px"><asp:Button ID="btnSil" runat="server" Text="Sil" Enabled="False" OnClientClick="return confirm('Silmek İstiyor musunuz?')" OnClick="btnSil_Click" /></td>
+                </tr>
+                <tr style="height:30px;">
+                    <td colspan="3"><asp:Label ID="lblMesaj" runat="server" Text=""></asp:Label> </td>
+                </tr>
+            </table>
+
+        </asp:Panel>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebEntityOkul/WebForm1.aspx.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebEntityOkul
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        OkulEntities ent = new OkulEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                GetAllSiniflar();
+            }
+
+        }
+
+        private void GetAllSiniflar()
+        {
+            var siniflar = (from s in ent.Sınıflar
+                            select new { s.Adı, s.Id }).ToList();
+
+            ddlSiniflar.DataTextField = "Adı";
+            ddlSiniflar.DataValueField = "Id";
+            ddlSiniflar.DataSource = siniflar;
+            ddlSiniflar.DataBind();
+
+            GetAllOgrencilerBySinifID(Convert.ToInt32(ddlSiniflar.SelectedValue));
+        }
+
+        protected void itemselected(object sender, EventArgs e)
+        {
+            Temizle();
+            GetAllOgrencilerBySinifID(Convert.ToInt32(ddlSiniflar.SelectedValue));
+        }
+
+        private void GetAllOgrencilerBySinifID(int sinifID)
+        {
+            var Ogrenciler = (from o in ent.Ogrencilers
+                              where o.SınıfId == sinifID
+                              select new { o.Ad, o.Soyad, o.Telefon, o.Adres, o.TC, o.TaksitSayısı, o.TaksitTutar, o.Id }).ToList();
+            gvOgrenciler.DataSource = Ogrenciler;
+            gvOgrenciler.DataBind();
+        }
+
+        protected void ddlSiniflar_DataBound(object sender, EventArgs e)
+        {
+        }
+
+        protected void gvOgrenciler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Temizle();
+            txtAdi.Text = HttpUtility.HtmlDecode(gvOgrenciler.SelectedRow.Cells[1].Text);
+            txtSoyadi.Text = gvOgrenciler.SelectedRow.Cells[2].Text;
+            txtTelefon.Text = gvOgrenciler.SelectedRow.Cells[3].Text;
+            txtAdres.Text = gvOgrenciler.SelectedRow.Cells[4].Text;
+            txtTC.Text = gvOgrenciler.SelectedRow.Cells[5].Text;
+            txtTSayisi.Text = gvOgrenciler.SelectedRow.Cells[6].Text;
+            txtTTutari.Text = gvOgrenciler.SelectedRow.Cells[7].Text;
+            txtId.Text = gvOgrenciler.SelectedValue.ToString();
+
+            OgrencilerPaneli.Visible = true;
+            btnDegistir.Enabled = true;
+            btnKaydet.Enabled = false;
+            btnSil.Enabled =true;
+        }
+
+        protected void lbtnEkle_Click(object sender, EventArgs e)
+        {
+            OgrencilerPaneli.Visible = true;
+            btnDegistir.Enabled = false;
+            btnKaydet.Enabled = true;
+            btnSil.Enabled = false;
+
+            Temizle();
+        }
+
+        private void Temizle()
+        {
+            txtAdi.Text = "";
+            txtSoyadi.Text = "";
+            txtTelefon.Text = "";
+            txtAdres.Text = "";
+            txtTC.Text = "";
+            txtTSayisi.Text = "";
+            txtTTutari.Text = "";
+            lblMesaj.Text = "";
+        }
+
+        protected void btnKaydet_Click(object sender, EventArgs e)
+        {
+            if(OgrenciKontrol())
+            {
+                lblMesaj.Text = "Öğrenci Zaten Kayıtlı!!!";
+                txtAdi.Focus();
+            }
+            else
+            {
+                Ogrenciler ogr = new Ogrenciler();
+                ogr.Ad = txtAdi.Text;
+                ogr.Soyad = txtSoyadi.Text;
+                ogr.Telefon = txtTelefon.Text;
+                ogr.Adres = txtAdres.Text;
+                ogr.TC = txtTC.Text;
+                ogr.TaksitSayısı = Convert.ToByte(txtTSayisi.Text);
+                ogr.TaksitTutar = Convert.ToDecimal(txtTTutari.Text);
+                ogr.SınıfId = Convert.ToInt32(ddlSiniflar.SelectedValue);
+                ent.Ogrencilers.Add(ogr);
+                try
+                {
+                    ent.SaveChanges();
+                    GetAllOgrencilerBySinifID(Convert.ToInt32(ddlSiniflar.SelectedValue));
+                    btnKaydet.Enabled = false;
+                    Temizle();
+                    lblMesaj.Text = "Öğrenci Kaydedildi";
+                }
+                catch (Exception ex)
+                {
+                    string hata = ex.Message;
+                }
+            }
+        }
+
+        private bool OgrenciKontrol()
+        {
+            int sayisi = (from o in ent.Ogrencilers
+                          where o.Ad == txtAdi.Text && o.Soyad == txtSoyadi.Text
+                          select o).ToList().Count();
+            return Convert.ToBoolean(sayisi);
+        }
+
+        protected void btnDegistir_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            //var degistir = ent.Ogrencilers.Where(o => o.Id == Convert.ToInt32(txtId.Text)).Select(o => o).First();
+            var degistir = (from o in ent.Ogrencilers
+                            where o.Id == id
+                            select o).ToList().FirstOrDefault();
+            degistir.Ad = txtAdi.Text;
+            degistir.Soyad = txtSoyadi.Text;
+            degistir.Telefon = txtTelefon.Text;
+            degistir.Adres = txtAdres.Text;
+            degistir.TC = txtTC.Text;
+            degistir.TaksitSayısı = Convert.ToByte(txtTSayisi.Text);
+            degistir.TaksitTutar = Convert.ToDecimal(txtTTutari.Text);
+            degistir.SınıfId = Convert.ToInt32(ddlSiniflar.SelectedValue);
+            try
+            {
+                ent.SaveChanges();
+                lblMesaj.Text = "Öğrenci Bilgileri Değiştirildi.";
+                GetAllOgrencilerBySinifID(Convert.ToInt32(ddlSiniflar.SelectedValue));
+                Temizle();
+                btnDegistir.Enabled = false;
+                btnSil.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                string hata = ex.Message;
+            }
+        }
+
+        protected void btnSil_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvOgrenciler_DataBound(object sender, EventArgs e)
+        {
+        }
+    }
+}
